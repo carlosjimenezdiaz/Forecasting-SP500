5e7c1fc1016fadf8cb91e03e2c6012c75affb4e4
carlosjimenezdiaz <carlos.jimenezdiaz@protonmail.com>
2022-04-23 21:36:49 +0200

    Create Forecasting Indexes.R

---
+++ Forecasting Indexes.R
@@ -0,0 +1,255 @@
+# Libraries that we need
+if (!require("tidyquant")) install.packages("tidyquant"); library(tidyquant)
+if (!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
+if (!require("RQuantLib")) install.packages("RQuantLib"); library(RQuantLib)
+
+# Local Variables
+Ticker      <- "^DJI" # ^GSPC -> SP500 Index / ^IXIC -> Nasdaq Index / ^DJI -> Downjones Index
+Ticker_Name <- "DownJones"
+top_years   <- 6 # How many highly correlated years wants to use to traing your models
+
+# Local Dataframes
+db_correl      <- NULL
+db_simulations <- NULL
+
+# Getting historical data from Yahoo Finance
+db_yahoo_data <- tq_get(Ticker,
+       from = Sys.Date() - lubridate::years(100),
+       to   = Sys.Date(),
+       get  = "stock.prices", 
+       complete_cases = TRUE) %>%
+  dplyr::select(date, adjusted) %>%
+  dplyr::mutate(Year = lubridate::year(date)) %>%
+  dplyr::filter(Year > 1927) %>% # Just for the SP500
+  dplyr::group_by(Year) %>%
+  dplyr::mutate(Ret = TTR::ROC(adjusted, n = 1, type = "discrete")) %>%
+  dplyr::ungroup() %>%
+  replace(is.na(.), 0)
+
+# Splitting the data into historical and current year
+cYear <- db_yahoo_data %>%
+  dplyr::filter(Year == lubridate::year(Sys.Date())) %>%
+  dplyr::group_by(Year) %>%
+  dplyr::mutate(Log_Ret  = log(1 + Ret),
+                Acum_Ret = cumsum(Log_Ret),
+                Dis_Ret  = exp(Acum_Ret) - 1,
+                col_line = "green",
+                num_day  = 1:n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(date, Year, Dis_Ret, col_line, num_day)
+
+hYear <- db_yahoo_data %>%
+  dplyr::filter(Year != lubridate::year(Sys.Date())) %>%
+  dplyr::group_by(Year) %>%
+  dplyr::mutate(Log_Ret  = log(1 + Ret),
+                Acum_Ret = cumsum(Log_Ret),
+                Dis_Ret  = exp(Acum_Ret) - 1,
+                col_line = "black",
+                num_day  = 1:n()) %>%
+  dplyr::mutate(nObs = n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(nObs > 200) %>% # We want to have a full year of data in the historical dataframe
+  dplyr::select(-nObs) %>%
+  dplyr::select(date, Year, Dis_Ret, col_line, num_day)
+
+# Plotting all the results
+hYear %>%
+  bind_rows(cYear) %>%
+  ggplot(aes(x = num_day, y = Dis_Ret, colour = col_line, group = Year)) +
+  geom_line() + 
+  scale_color_manual(values = c("gray", "blue")) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title    = str_glue("Historical Performance of the {Ticker_Name} vs Current Year Performance. Ticker {Ticker}."),
+       subtitle = str_glue("Pattern detection and seasonality analysis. Analysis for the {cYear$date %>% tail(n = 1)}"),
+       caption  = "By: Carlos Jimenez",
+       x = "Days in a Year",
+       y = "Accumulated Return") +
+  theme(legend.position = "none") + 
+  geom_hline(yintercept = 0, 
+             linetype   = "dotted", 
+             color      = "darkgreen",  
+             size       = 1)
+
+# Plotting the YTD return up to the date of the analysis
+hYear %>%
+  bind_rows(cYear) %>%
+  dplyr::group_by(Year) %>%
+  dplyr::filter(num_day <= cYear$num_day %>% tail(n = 1)) %>%
+  dplyr::filter(num_day == max(num_day)) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(Year, Dis_Ret) %>%
+  dplyr::mutate(Year = factor(Year, levels = Year)) %>%
+  ggplot(aes(x = Year, y = Dis_Ret)) +
+  geom_bar(stat = "identity") + 
+  scale_color_manual(values = c("lightgray", "green")) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title    = str_glue("What if Analysis: Historical Performance of the {Ticker_Name} vs Current Year Performance. Ticker {Ticker}."),
+       subtitle = str_glue("Today's YTD ({cYear$date %>% tail(n = 1)}) vs Historical Same Date."),
+       caption  = "By: Carlos Jimenez",
+       x = "",
+       y = "Accumulated Return") + 
+  geom_hline(yintercept = cYear$Dis_Ret %>% tail(n = 1), 
+             linetype   = "dotted", 
+             color      = "blue",  
+             size       = 1) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+# Analyzing the Beta Correlation level between past years performance and current year
+cYear_xts <- cYear %>% 
+  dplyr::select(date, Dis_Ret) %>% 
+  column_to_rownames(var = "date") %>% 
+  as.xts()
+
+for(i in seq(hYear$Year %>% first(), (hYear$Year %>% last()) - 1, 1)){ # i <- 1932
+  
+  # Subsetting the Historical dataset by Year and by Number of days in cYear
+  hYear_xts <- hYear %>% dplyr::filter(Year == i) %>% 
+    dplyr::slice(1:nrow(cYear_xts)) %>% 
+    dplyr::select(date, Dis_Ret) %>% 
+    dplyr::mutate(date = index(cYear_xts)) %>%
+    column_to_rownames(var = "date") %>% 
+    as.xts()
+
+  # Saving the info
+  db_correl <- db_correl %>%  
+    bind_rows(data.frame(Year = i,
+                         Corr = PerformanceAnalytics::table.Correlation(cYear_xts, hYear_xts)$Correlation))
+}
+
+# Getting the years with the most correlated performance vs current year
+db_correl_top <- db_correl %>%
+  dplyr::arrange(desc(Corr)) %>%
+  dplyr::top_n(ifelse(top_years > 7, 7, top_years), wt = Corr)
+
+# Plotting the most correlated years vs current years
+data_chart <- hYear %>%
+  dplyr::filter(Year %in% db_correl_top$Year) %>%
+  bind_rows(cYear) %>%
+  dplyr::mutate(Year = Year %>% as.character())
+
+data_chart %>%
+  ggplot(aes(x = num_day, y = Dis_Ret, colour = Year, group = Year)) +
+  geom_line(data = filter(data_chart, Year != Sys.Date() %>% lubridate::year() %>% as.character()), size = 0.8, alpha = 0.5) + 
+  geom_line(data = filter(data_chart, Year == Sys.Date() %>% lubridate::year() %>% as.character()), size = 1.1) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title    = "Most correlated Past Year Performance and Current Year Performance. Ticker {Ticker}.",
+       subtitle = str_glue("Analysis done on the {Ticker_Name}"),
+       caption  = "By: Carlos Jimenez",
+       x = "Days in a Year",
+       y = "Accumulated Return") +
+  geom_hline(yintercept = 0, 
+             linetype   = "dotted", 
+             color      = "darkgreen",  
+             size       = 1) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank()) +
+  scale_colour_manual(values=c("gray", "blue", "green", "black", "orange", "purple", "steelblue", "red"))
+
+# Training and forecasting multiple models (using multiple years as regressors)
+for(i in 1:(db_correl_top$Year %>% length())){ # i <- 2
+  
+  # Subsetting the Years
+  hist_years <- db_correl_top %>% 
+    dplyr::slice(1:i) %>%
+    dplyr::select(Year) %>%
+    pull(1)
+  
+  # Training a model to forecast where the Underlying will end
+  db_training <- hYear %>%
+    dplyr::filter(Year %in% db_correl_top$Year) %>%
+    bind_rows(cYear) %>%
+    dplyr::filter(Year %in% c(hist_years, lubridate::year(Sys.Date()))) %>%
+    dplyr::select(num_day, Dis_Ret, Year) %>%
+    spread(Year, Dis_Ret) %>%
+    na.omit() %>%
+    dplyr::select(-num_day) %>%
+    rename("Label" = lubridate::year(Sys.Date()) %>% as.character())
+  
+  MLRM_Model = lm(formula = Label ~ ., data = db_training)
+  
+  # Predicting the Test set results
+  db_test <- hYear %>%
+    dplyr::filter(Year %in% db_correl_top$Year) %>%
+    bind_rows(cYear) %>%
+    dplyr::filter(Year %in% c(hist_years, lubridate::year(Sys.Date()))) %>%
+    dplyr::select(num_day, Dis_Ret, Year) %>%
+    spread(Year, Dis_Ret) %>%
+    dplyr::slice(nrow(db_training) +1 :n()) %>%
+    column_to_rownames(var = "num_day") %>%
+    dplyr::select(-c(lubridate::year(Sys.Date()) %>% as.character())) %>%
+    na.omit()
+  
+  y_pred = predict(MLRM_Model, newdata = db_test)
+  
+  # Adjusting the forecast
+  First_Forecast_Observation   <- y_pred %>% first()
+  First_Historical_Observation <- cYear %>% dplyr::select(Dis_Ret) %>% tail(n = 1)
+  Delta <- (First_Historical_Observation - First_Forecast_Observation) %>% pull(1)
+  
+  # Saving Simulations
+  db_simulations <- db_simulations %>%
+    bind_rows(data.frame(num_day  = y_pred %>% names() %>% as.numeric(),
+                         Dis_Ret  = y_pred + Delta,
+                         ID_Model = i))
+}
+
+# Plotting the results
+First_Price <- db_yahoo_data %>%
+  dplyr::filter(Year == lubridate::year(Sys.Date())) %>%
+  dplyr::select(adjusted) %>%
+  pull(1) %>%
+  first()
+
+First_Date <- db_yahoo_data %>%
+  dplyr::filter(Year == lubridate::year(Sys.Date())) %>%
+  dplyr::select(date) %>%
+  pull(1) %>%
+  first()
+
+Last_Date <- db_yahoo_data %>%
+  dplyr::filter(Year == lubridate::year(Sys.Date())) %>%
+  dplyr::select(date) %>%
+  pull(1) %>%
+  last()
+
+  # Adapting historical database
+  db_cYear_enhanced <- cYear %>%
+    dplyr::select(date, Dis_Ret) %>%
+    dplyr::mutate(ID_Model = 0)
+  
+  db_simulations_enhanced <- db_simulations %>%
+    dplyr::group_by(ID_Model) %>%
+    dplyr::mutate(num_day = 1:n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(date = Last_Date + num_day) %>%
+    dplyr::select(c(date, Dis_Ret, ID_Model, -num_day)) %>%
+    dplyr::group_by(date) %>%
+    dplyr::summarise(min_Ret  = min(Dis_Ret),
+                     mean_Ret = mean(Dis_Ret),
+                     max_Ret  = max(Dis_Ret)) %>%
+    dplyr::filter(date < "2022-12-20")
+
+db_cYear_enhanced %>%
+  ggplot(aes(x = date, y = Dis_Ret)) +
+  geom_line(colour = "darkgray", size = 0.8) +
+  geom_line(data = db_simulations_enhanced, aes(x = date, y = min_Ret), colour = "blue", linetype  = "dashed", size = 0.5) +
+  geom_line(data = db_simulations_enhanced, aes(x = date, y = mean_Ret), colour = "black", size = 0.8) +
+  geom_line(data = db_simulations_enhanced, aes(x = date, y = max_Ret), colour = "blue", linetype  = "dashed", size = 0.5) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title    = str_glue("Future possible scenario of the {Ticker_Name}"),
+       subtitle = "Using a Multiple Linear Regression Model.",
+       caption  = "By: Carlos Jimenez",
+       x = "",
+       y = "Accumulated Return") +
+  theme(legend.position = "none") + 
+  geom_vline(xintercept = Last_Date, 
+             linetype   = "dotted", 
+             color      = "black", 
+             size       = 0.5) + 
+  geom_hline(yintercept = 0, 
+             linetype   = "dashed", 
+             color      = "gray", 
+             size       = 0.5) + 
+  scale_x_date(date_labels = "%m-%d-%Y")
+
